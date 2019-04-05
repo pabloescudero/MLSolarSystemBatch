21eab295e05398b3c1ff7cb53dce13ce064cb5d4
pabloescudero <pablo.david.escudero@gmail.com>
2019-04-05 15:18:58 -0300

    First Commit

---
+++ SSBatch/src/main/java/com/SolarSystem/Batch/SSBatch/App.java
@@ -0,0 +1,13 @@
+package com.SolarSystem.Batch.SSBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
